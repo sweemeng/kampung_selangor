11e2db8f1b85feafee77a3f20d1dea4c2a53f067
sweemeng <sweester@gmail.com>
2017-09-29 21:52:58 +0800

    Add scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,18 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+from bs4 import Beautiful
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+r = requests.get("http://www2.selangor.gov.my/gombak.php/pages/view/20?mid=290")data = r.content
+soup = BeautifulSoup(data, "html.parser")
+content=soup.find(id="container_content")
+td = content.find("table").find_all("td")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+for i in td:
+    temp = i.find(text=re.compile("^Nama Kampung"))
+    if temp:
+        kampung = temp.split(":")[1].replace("\xa0", ""))
+        link = "http://www2.selangor.gov.my/{url}".format(url=i.find("a")["href"])
+        scraperwiki.sqlite.save(unique_keys=['name'], data={"name": kampung, "link": link})
+
+
+
