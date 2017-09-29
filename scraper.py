075f7178ce0b5162d93c87e596b7bf6737a20d14
sweemeng <sweester@gmail.com>
2017-09-29 21:56:54 +0800

    Fix error

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import Beautiful
+from bs4 import BeautifulSoup
 import scraperwiki
 
 r = requests.get("http://www2.selangor.gov.my/gombak.php/pages/view/20?mid=290")
@@ -11,7 +11,7 @@ td = content.find("table").find_all("td")
 for i in td:
     temp = i.find(text=re.compile("^Nama Kampung"))
     if temp:
-        kampung = temp.split(":")[1].replace("\xa0", ""))
+        kampung = temp.split(":")[1].replace("\xa0", "")
         link = "http://www2.selangor.gov.my/{url}".format(url=i.find("a")["href"])
         scraperwiki.sqlite.save(unique_keys=['name'], data={"name": kampung, "link": link})
 
